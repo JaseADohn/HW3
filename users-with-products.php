afb92cb7019ba4f7e3315bccac465a8dc00b267e
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-10-03 13:15:30 -0500

    Update users-with-products.php

--- users-with-products.php
+++ users-with-products.php
@@ -1,1 +1,11 @@
+<?php
+require_once("util-db.php");
+require_once("users-with-products.php");
+$pageTitle = "Users";
+include "view-header.php";
 
+$users = selectUsers();
+include "view-users-with-products.php";
+
+include "view-footer.php";
+?>
