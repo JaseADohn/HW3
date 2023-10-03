d1783b930aac524a49e687c3a587aa9f9b0e9509
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-10-03 11:53:31 -0500

    Update model-category-by-product.php

--- model-category-by-product.php
+++ model-category-by-product.php
@@ -2,7 +2,7 @@
 function selectCategoryByProduct($pid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT p.product_id, product_name, p.category_id, c.category_name, orders_amount, order_status FROM `product` p join orders o on o.product_id = p.product_id JOIN category c ON c.category_id = p.category_id where c.category_id=?");
+        $stmt = $conn->prepare("SELECT p.product_id, product_name, p.category_id, c.category_name, orders_amount, order_status FROM `product` p join orders o on o.product_id = p.product_id JOIN category c ON c.category_id = p.category_id where p.category_id=?");
         $stmt->bind_param("i", $pid);
         $stmt->execute();
         $result = $stmt->get_result();
