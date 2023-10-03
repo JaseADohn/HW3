1f5e787c8c5c2c92d51e91b15c006d4cc943a2dc
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-10-03 11:41:19 -0500

    Update model-category-by-product.php

--- model-category-by-product.php
+++ model-category-by-product.php
@@ -1,1 +1,16 @@
-
+<?php
+function selectCategoryByProduct($uid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT p.product_id, product_name, category_id, orders_amount, order_status FROM `product` p join orders o on o.product_id = p.product_id where o.user_id=?");
+        $stmt->bind_param("i", $uid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
