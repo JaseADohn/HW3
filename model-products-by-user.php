60c4b367f02e0990e4cdf9e9411eeef9fa3431ac
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-10-03 12:17:53 -0500

    Update model-products-by-user.php

--- model-products-by-user.php
+++ model-products-by-user.php
@@ -3,7 +3,7 @@ function selectProductsByUser($uid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT p.product_id, product_name, category_id, orders_amount, order_status FROM `product` p join orders o on o.product_id = p.product_id where o.user_id=?");
-        $stmt->bind_param("i", $uid);
+        $stmt->bind_param("s", $uid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
