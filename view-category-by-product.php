f898af124c05578f9ba5da0990688eecddab9611
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-10-03 11:56:01 -0500

    Update view-category-by-product.php

--- view-category-by-product.php
+++ view-category-by-product.php
@@ -1,1 +1,30 @@
+<h1>Products by user</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>Product ID</th>
+      <th>Name</th>
+      <th>Category Name</th>
+      <th>Order Amount</th>
+      <th>Order Status</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($category = $categorys->fetch_assoc()) {
+  ?>
+  <tr>
+    <td><?php echo $product['product_id']; ?></td>
+    <td><?php echo $product['product_name']; ?></td>
+    <td><?php echo $product['category_name']; ?></td>
+    <td><?php echo $product['orders_amount']; ?></td>
+    <td><?php echo $product['order_status']; ?></td>
+  </tr>
+  <?php
+}
 
+?>
+    </tbody>
+  </table>
+</div>
