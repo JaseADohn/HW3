5a01a9c2691eb787b98751750929ea2ec3ef85ce
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-10-03 13:36:34 -0500

    Update view-users-with-products.php

--- view-users-with-products.php
+++ view-users-with-products.php
@@ -7,14 +7,16 @@ while ($user = $users->fetch_assoc()) {
     <div class="card-body">
       <h5 class="card-title"><?php echo $user['user_name']; ?></h5>
       <p class="card-text">
+      <ul class="list-group">
   <?php
     $productss = selectProductsbyuser($user['user_id']);
     while($product = $productss->fetch_assoc()) {
       ?>
-      
+        <li class="list-group-item"><?php echo $product['product_name']; ?> - <?php echo $product['product_id']; ?></li>
       <?php
     }
   ?>
+      </ul>
       </p>
       <p class="card-text"><small class="text-body-secondary">Password: <?php echo $user['user_password']; ?></small></p>
     </div>
