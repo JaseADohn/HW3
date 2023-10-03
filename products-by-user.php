d8c98bbd1b53205d54725ceb06c69c05e32d1104
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-10-03 11:04:16 -0500

    Update products-by-user.php

--- products-by-user.php
+++ products-by-user.php
@@ -1,11 +1,11 @@
 <?php
 require_once("util-db.php");
-require_once("model-products.php");
-$pageTitle = "Products";
+require_once("model-products-by-user.php");
+$pageTitle = "products by user";
 include "view-header.php";
 
-$products = selectProducts();
-include "view-products.php";
+$products = selectProductsByUser($_GET['id']);
+include "view-products-by-user.php";
 
 include "view-footer.php";
 ?>
