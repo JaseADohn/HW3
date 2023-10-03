47f929230214b40b3b8406ba5415a44c1862035f
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-10-03 13:14:15 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -26,7 +26,7 @@
           <a class="nav-link" href="products.php">Products</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="users--with-products.php">Users with Products</a>
+          <a class="nav-link" href="users-with-products.php">Users with Products</a>
         </li>
       </ul>
     </div>
