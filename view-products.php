4e9210627fafcc32c7e4ddccd90c3aae54c879f7
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-10-03 10:50:28 -0500

    Update view-products.php

--- view-products.php
+++ view-products.php
@@ -1,1 +1,24 @@
+<h1>Users</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>Name</th>
+      <th>Category ID</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($user = $users->fetch_assoc()) {
+  ?>
+  <tr>
+    <td><?php echo $user['product_name']; ?></td>
+    <td><?php echo $user['category_id']; ?></td>
+  </tr>
+  <?php
+}
 
+?>
+    </tbody>
+  </table>
+</div>
