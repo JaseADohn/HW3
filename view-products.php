38d90320b1da9aa98c6db0d97f09d1801860a81e
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-10-03 12:07:05 -0500

    Update view-products.php

--- view-products.php
+++ view-products.php
@@ -16,7 +16,7 @@ while ($product = $products->fetch_assoc()) {
     <td><?php echo $product['product_name']; ?></td>
     <td><?php echo $product['category_id']; ?></td>
     <td>
-      <form method="post" action="categorys-by-products.php">
+      <form method="post" action="categorys-by-product.php">
   <input type="hidden" name="pid" value="<?php echo $product['category_id']; ?>">
   <button type="category" class="btn btn-primary">Category</button>
       </form>
