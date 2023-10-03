2df81438c616671dd23aa3e4dc34404583c1ea24
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-10-03 11:40:37 -0500

    Update categorys-by-product.php

--- categorys-by-product.php
+++ categorys-by-product.php
@@ -1,11 +1,11 @@
 <?php
 require_once("util-db.php");
-require_once("model-products-by-user.php");
-$pageTitle = "products by user";
+require_once("model-category-by-product.php");
+$pageTitle = "Category by product";
 include "view-header.php";
 
-$products = selectProductsByUser($_GET['id']);
-include "view-products-by-user.php";
+$products = selectCategoryByProduct($_POST['pid']);
+include "view-category-by-product.php";
 
 include "view-footer.php";
 ?>
