52892d3463b640894e5fb54d63a26a5047bd0c80
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-10-03 10:45:53 -0500

    Update model-products.php

--- model-products.php
+++ model-products.php
@@ -1,1 +1,15 @@
-
+<?php
+function selectProducts() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT user_id, user_name, user_password FROM `user` ");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
