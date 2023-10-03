e9ac203d7d65ada1c50a324378b77df5dd01ae64
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-10-03 10:45:14 -0500

    Create products.php

---
+++ products.php
@@ -0,0 +1,11 @@
+<?php
+require_once("util-db.php");
+require_once("model-products.php");
+$pageTitle = "Products";
+include "view-header.php";
+
+$users = selectProducts();
+include "view-products.php";
+
+include "view-footer.php";
+?>
