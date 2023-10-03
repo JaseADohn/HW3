2b736b149bd05a057fc60d67ec0c87e01bf2c244
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-10-03 13:18:29 -0500

    Update model-users-with-products.php

--- model-users-with-products.php
+++ model-users-with-products.php
@@ -12,4 +12,20 @@ function selectUsers() {
         throw $e;
     }
 }
+
+function selectProductsByUser($uid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT p.product_id, product_name, p.category_id, c.category_name, orders_amount, order_status FROM `product` p join orders o on o.product_id = p.product_id JOIN category c ON c.category_id = p.category_id where o.user_id=?");
+        $stmt->bind_param("i", $uid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
+
