f3f332bca0c8cf2da39e52ecff605ed50afd2b29
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-10-23 18:12:06 -0500

    Update users.php

--- users.php
+++ users.php
@@ -1,11 +1,10 @@
 <?php
 require_once("util-db.php");
 require_once("model-users.php");
+
 $pageTitle = "Users";
 include "view-header.php";
-
 $users = selectUsers();
 include "view-users.php";
-
 include "view-footer.php";
 ?>
