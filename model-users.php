39be6a65b4e8167eccb27b9ab375c810f9fb9a69
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-10-23 17:39:15 -0500

    Update model-users.php

--- model-users.php
+++ model-users.php
@@ -12,4 +12,48 @@ function selectUsers() {
         throw $e;
     }
 }
+
+function insertUser($uName, $uPassword) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `user` (`user_name`, `user_password`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $uName, $uPassword);
+        $success = $stmt->execute();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateUser($uName, $uPassword, $uid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `user` set (`user_name` = ?, `user_password` = ?) where user_id = ?");
+        $stmt->bind_param("ssi", $uName, $uPassword, $uid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteUser($uid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from user where user_id=?");
+        $stmt->bind_param("i", $uid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
 ?>
