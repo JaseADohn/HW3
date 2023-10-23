76f0d84e3bc3daf24ad0409052cbb91500e3c2c3
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-10-23 18:26:53 -0500

    Update users.php

--- users.php
+++ users.php
@@ -8,7 +8,7 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
     switch ($_POST['actionType']) {
       case "Add":
-      if (insertUser($_POST['uName'], $_POST['uPassword']);) {
+      if (insertUser($_POST['uName'], $_POST['uPassword'])) {
           echo '<div class="alert alert-success" role="alert"> User added </div>';
         } else {
           echo '<div class="alert alert-danger" role="alert"> error </div>';
