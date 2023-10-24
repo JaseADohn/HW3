27468dc3ca2b64143dfc2876d263dca7f25f7cf7
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-10-23 20:18:29 -0500

    Update view-users.php

--- view-users.php
+++ view-users.php
@@ -16,6 +16,8 @@
       <th>Name</th>
       <th>Password</th>
       <th></th>
+      <th></th>
+      <th></th>
       </tr>
     </thead>
     <tbody>
@@ -27,6 +29,21 @@ while ($user = $users->fetch_assoc()) {
     <td><?php echo $user['user_name']; ?></td>
     <td><?php echo $user['user_password']; ?></td>
     <td><a href ="products-by-user.php?id=<?php echo $user['user_id']; ?>">Products</a></td>
+    <td>
+      <form method="post" action="">
+  <input type="hidden" name="uid" value="<?php echo $user['user_id']; ?>">
+  <input type = "hidden" name = "actiontype" value = "Delete">
+  <button type="category" class="btn btn-primary">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+        </svg>
+  </button>
+      </form>
+    </td>
+    <td>
+      
+    </td>
   </tr>
   <?php
 }
