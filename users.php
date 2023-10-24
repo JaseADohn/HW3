5d15801f7e3e4d438548c722fbfe8fbdf6170315
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-10-23 20:58:38 -0500

    Update users.php

--- users.php
+++ users.php
@@ -12,6 +12,11 @@ if (isset($_POST['actionType'])) {
           echo '<div class="alert alert-success" role="alert"> User added </div>';
         } 
         break;
+        case "Edit":
+      if (updateUser($_POST['uName'], $_POST['uPassword'], $_POST['uid'])) {
+          echo '<div class="alert alert-success" role="alert"> User Edited </div>';
+        } 
+        break;
     case "Delete":
       if (deleteUser($_POST['uid'])) {
           echo '<div class="alert alert-success" role="alert"> Course deleted </div>';
