51e04f91740a410ea4a78748ce9e0e3161a5d6ff
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-10-24 22:03:00 -0500

    Update model-products.php

--- model-products.php
+++ model-products.php
@@ -13,6 +13,20 @@ function selectProducts() {
     }
 }
 
+function selectCategoriesForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT category_id FROM `product` ");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 function insertProduct($pName, $cid) {
     try {
         $conn = get_db_connection();
