03d667b8e11b6b1b1803ce2631060850a76f154b
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-10-24 19:50:37 -0500

    Update products.php

--- products.php
+++ products.php
@@ -4,6 +4,26 @@ require_once("model-products.php");
 $pageTitle = "Products";
 include "view-header.php";
 
+if (isset($_POST['actionType'])) {
+    switch ($_POST['actionType']) {
+    case "Add":
+      if (insertProduct($_POST['pName'], $_POST['cid'])) {
+          echo '<div class="alert alert-success" role="alert"> Product added </div>';
+        } 
+        break;
+        case "Edit":
+      if (updateProduct($_POST['pName'], $_POST['cid'], $_POST['pid'])) {
+          echo '<div class="alert alert-success" role="alert"> Product Edited </div>';
+        } 
+        break;
+    case "Delete":
+      if (deleteProduct($_POST['pid'])) {
+          echo '<div class="alert alert-success" role="alert"> Product deleted </div>';
+        } 
+        break;
+    }
+}
+
 $products = selectProducts();
 include "view-products.php";
 
